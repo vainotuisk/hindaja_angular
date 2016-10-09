43433c3688b8a93cb5c34183ca05b1e808a482de
vainotuisk <vainotuisk@gmail.com>
2016-10-09 21:02:12 +0300

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+(function () {
+'use strict';
+angular.module('hindajaAngular',[])
+.controller("NarrowItDownController",NarrowItDownController)
+.service("MenuSearchService",MenuSearchService)
+
+
+
+}]);
